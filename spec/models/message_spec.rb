8e6b2c3df9d4bcf8e831abf4c4700b24317c2794
shimpei kasamatsu <ogress08@gmail.com>
2017-05-16 22:31:42 +0900

    modeltest fix

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 describe Message do
+  let(:user) do
+    create(:user)
+  end
+
+  let(:group) do
+    create(:group)
+  end
+
   describe '#create' do
     it 'is valid with body, group_id, user_id' do
-      user    = create(:user)
-      group   = create(:group)
       message = build(:message, group_id: group.id, user_id: user.id)
       expect(message).to be_valid
     end
 
     it 'is invalid without a body' do
-      user    = create(:user)
-      group   = create(:group)
       message = build(:message, body: nil, group_id: group.id, user_id: user.id)
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
     end
 
     it "is invalid without a group" do
-      user    = create(:user)
       message = build(:message, user_id: user.id, group_id: nil)
       message.valid?
       expect(message.errors[:group_id]).to include("を入力してください")
     end
 
     it "is invalid without a user" do
-      group   = create(:group)
       message = build(:message, group_id: group.id, user_id: nil)
       message.valid?
       expect(message.errors[:user_id]).to include("を入力してください")
