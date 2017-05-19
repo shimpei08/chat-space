064cefcb2180f8522cd326d2ae7eb89d574cfa19
shimpei kasamatsu <ogress08@gmail.com>
2017-05-19 17:13:38 +0900

    fix controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,8 +1,9 @@
-require 'rails_helper'
+require 'rails_helper' 
+
 describe MessagesController do
   let(:message) { create(:message) }
-  let(:group)   { create(:group)}
-  let(:user)    { create(:user)}
+  let(:group)   { create(:group) }
+  let(:user)    { create(:user) }
 
   describe 'GET #new' do
     before do
@@ -14,7 +15,7 @@ describe MessagesController do
     end
     it "populates an array of groups ordered by created_at desc " do
       groups = user.groups
-      expect(assigns(:groups)).to match(groups.sort{ |a,b| b.created_at <=> a.created_at })
+      expect(assigns(:groups)).to match(groups.order("created_at DESC"))
     end
 
     it "assigns the requested group to @group" do
@@ -23,7 +24,7 @@ describe MessagesController do
 
     it "populates an array of messages ordered by created_at desc" do
       messages = group.messages
-      expect(assigns(:messages)).to match(messages.sort{ |a,b| b.created_at <=> a.created_at})
+      expect(assigns(:messages)).to match(messages.order("created_at DESC"))
     end
 
     it "renders the :new template" do
@@ -55,7 +56,7 @@ describe MessagesController do
     end
 
     context "with valid attributes" do
-      let(:message_params) {post :create, params: {message: attributes_for(:message), group_id: group.id}}
+      let(:message_params) { post :create, params: { message: attributes_for(:message), group_id: group.id } }
       it "saves the new message in the database" do
         expect{ message_params }.to change(Message, :count).by(1)
       end
@@ -67,7 +68,7 @@ describe MessagesController do
     end
 
     context "with invalid attributes" do
-      let(:message_params) {post :create, params: {message: attributes_for(:message, {body: nil}), group_id: group.id}}
+      let(:message_params) { post :create, params: {message: attributes_for(:message, { body: nil }), group_id: group.id } }
       it "can't save the new message in the database" do
         message_params
         expect{ message_params }.not_to change(Message, :count)
