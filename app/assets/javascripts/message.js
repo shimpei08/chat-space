f670c8d1bab9550786729117838d33edb45e8a70
shimpei kasamatsu <ogress08@gmail.com>
2017-05-26 13:02:25 +0900

    message ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,39 @@
+$(function() {
+  function buildHTML(message) {
+    var html =
+    `<li class = content__list>
+    <h2 class="message__name"> ${message.name} </h2>
+    <h4 class="message__created_at"> ${ message.created_at } </h4>
+    <p class="message__body"> ${ message.body } </p>
+    </li>`
+    return html;
+  }
+
+  $('.comment').on('submit', function(e) {
+// 次の動作をストップ
+    e.preventDefault();
+// 連続更新を可能に
+    $('.comment__submit').removeAttr('data-disable-with');
+//  formdataを一括で送る
+    var message = new FormData($('.comment').get(0));
+    var current_group_id = $('.main').data('group-id')
+    $.ajax({
+      type:        'POST',
+      url:         '/groups/'+ current_group_id+ '/messages',
+      data:        message,
+      dataType:    'json',
+// new Formdataを使うため
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      html = buildHTML(data);
+      $('.content').prepend(html)
+      debugger;
+      $('.comment__message').val('');
+    })
+    .fail(function(data) {
+      alert('error');
+    });
+  });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,13 +13,24 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.new(create_params)
-    unless @message.save
+    if @message.save
+      respond_to do |format|
+        format.json { render json: message_js(@message) }
+      end
+    else
       flash[:alert] = 'メッセージを送信できませんでした'
+      redirect_to new_group_message_url
     end
-    redirect_to new_group_message_url
   end
 
   private
+
+  def message_js(message)
+    hash = {
+    name: message.user.name,
+    created_at: message.created_at.strftime("%Y/%m/%d/ %H:%M:%S"),
+    body: message.body, id: message.id }
+  end
 
   def create_params
     params.require(:message).permit(:body, :image).merge( group_id: params[:group_id] )
